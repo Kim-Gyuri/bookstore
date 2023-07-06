1c5f598752f2b380a3114317f5ecfbd1ff993588
Kim-Gyuri <gyuring0203@gmail.com>
2023-07-06 19:16:04 +0900

    테스트를 위한 세팅

--- src/test/java/springstudy/bookstore/BookstoreApplicationTests.java
+++ src/test/java/springstudy/bookstore/BookstoreApplicationTests.java
@@ -1,9 +1,8 @@
 package springstudy.bookstore;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BookstoreApplicationTests {
 
 	@Test
